55519d10b48a519862bb8fc9c8d73b5d1b2b903c
ThomasFeldman <tfeldman1.31@gmail.com>
2020-05-30 23:05:43 -0500

    adding some Tetrominos

--- tetris/app.js
+++ tetris/app.js
@@ -1,8 +1,30 @@
 document.addEventListener('DOMContentLoaded', () =>{
     const grid = document.querySelector('.grid');
     let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-button');
     const width = 10;
 
-    console.log(squares);
 
+    //The Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [width*2, width+1, width*2+1, width+2],
+        [0, width, width+1, width*2+1],
+        [width*2, width+1, width*2+1, width+2],
+        [0, width, width+1, width*2+1]
+    ]
+
+    const tTetromino = [
+        [],
+        [],
+        [],
+        []
+    ]
 })
